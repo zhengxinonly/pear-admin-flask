fcf2ad232e4c23632c4a5a261d6d62205f1517ba
zhengxinonly <pyxxponly@gmail.com>
2023-10-31 20:52:32 +0800

    feature: update flask to 3.0.0

--- pear_admin/apis/rights.py
+++ pear_admin/apis/rights.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required
 
@@ -10,29 +12,59 @@ rights_api = Blueprint("rights", __name__)
 @rights_api.get("/rights")
 @jwt_required()
 def rights_list():
-    page = request.args.get("page", type=int, default=1)
-    per_page = request.args.get("per_page", type=int, default=10)
+    t = request.args.get("type", default="")
+    if t == "tree":  # tree 组件数据，用于权限赋值
+        # 1. 获取所有的权限
+        rights_all = db.session.execute(db.select(RightsORM)).scalars()
+        rights_list = [
+            {"id": r.id, "pid": r.pid, "title": r.name, "sort": r.sort}
+            for r in rights_all
+        ]
+        # 2. 获取已有权限 id 集合
+        # 3. 列表转属性组件
+        rights_list.sort(key=lambda item: (item["pid"], item["id"]), reverse=True)
+        tree_dict = {}
+        for rights_dict in rights_list:  # 遍历子节点
+            # 2. 如果当前阶段已经存在于树状表格字典，则是父节点
+            if rights_dict["id"] in tree_dict.keys():
+                # 将之前的节点添加到父节点之下
+                rights_dict["children"] = deepcopy(tree_dict[rights_dict["id"]])
+                rights_dict["children"].sort(key=lambda item: item["sort"])
+                del tree_dict[rights_dict["id"]]
 
-    q = db.select(RightsORM).where(RightsORM.type == "menu")
-    pages = db.paginate(q, page=page, per_page=per_page, error_out=False)
+            # 1. 如果父节点未出现在树状字典里面，就新增子节点列表，否则就追加
+            if rights_dict["pid"] not in tree_dict.keys():
+                tree_dict[rights_dict["pid"]] = [rights_dict]
+            else:
+                tree_dict[rights_dict["pid"]].append(rights_dict)
 
-    ret = []
+        return {"code": 0, "data": tree_dict.get(0)}
+    if t == "treetable":  # tree table 数据
+        page = request.args.get("page", type=int, default=1)
+        per_page = request.args.get("per_page", type=int, default=10)
 
-    for page in pages.items:
-        data = page.json()
-        data["children"] = []
-        for child in page.children:
-            child_data = child.json()
-            if child.children:
-                child_data["children"] = [
-                    sub_child.json() for sub_child in child.children
-                ]
-                child_data["isParent"] = True
+        q = db.select(RightsORM).where(RightsORM.type == "menu")
+        pages = db.paginate(q, page=page, per_page=per_page, error_out=False)
 
-            data["children"].append(child_data)
-            data["isParent"] = True
-        ret.append(data)
-    return {"code": 0, "message": "请求权限数据成功", "count": pages.total, "data": ret}
+        ret = []
+
+        # 构建树状表格的数据
+        for page in pages.items:
+            data = page.json()
+            data["children"] = []
+            for child in page.children:
+                child_data = child.json()
+                if child.children:
+                    child_data["children"] = [
+                        sub_child.json() for sub_child in child.children
+                    ]
+                    child_data["isParent"] = True
+
+                data["children"].append(child_data)
+                data["isParent"] = True
+            ret.append(data)
+        return {"code": 0, "message": "请求权限数据成功", "count": pages.total, "data": ret}
+    return {"code": 0, "message": "请求权限数据成功，"}
 
 
 @rights_api.post("/rights")
--- pear_admin/apis/role.py
+++ pear_admin/apis/role.py
@@ -59,44 +59,12 @@ def role_rights(rid):
     role: RoleORM = db.session.execute(
         db.select(RoleORM).where(RoleORM.id == rid)
     ).scalar()
-
-    t = request.args.get("type")
-    if t == "tree":
-        # 1. 获取所有的权限
-        rights_all = db.session.execute(db.select(RightsORM)).scalars()
-        rights_list = [
-            {"id": r.id, "pid": r.pid, "title": r.name, "sort": r.sort}
-            for r in rights_all
-        ]
-        # 2. 获取已有权限 id 集合
-        own_rights_set = {r.id for r in role.rights_list}
-        # 3. 列表转属性组件
-        rights_list.sort(key=lambda item: (item["pid"], item["id"]), reverse=True)
-        tree_dict = {}
-        for rights_dict in rights_list:  # 遍历子节点
-            # 2. 如果当前阶段已经存在于树状表格字典，则是父节点
-            if rights_dict["id"] in tree_dict.keys():
-                # 将之前的节点添加到父节点之下
-                rights_dict["children"] = deepcopy(tree_dict[rights_dict["id"]])
-                rights_dict["children"].sort(key=lambda item: item["sort"])
-                del tree_dict[rights_dict["id"]]
-
-            # 1. 如果父节点未出现在树状字典里面，就新增子节点列表，否则就追加
-            if rights_dict["pid"] not in tree_dict.keys():
-                if rights_dict["id"] in own_rights_set:
-                    rights_dict["checked"] = True
-                tree_dict[rights_dict["pid"]] = [rights_dict]
-            else:
-                if rights_dict["id"] in own_rights_set:
-                    rights_dict["checked"] = True
-                tree_dict[rights_dict["pid"]].append(rights_dict)
-
-        return {"code": 0, "data": tree_dict.get(0)}
+    own_rights_list = [r.id for r in role.rights_list]
 
     return {
         "code": 0,
         "message": "返回角色权限数据成功",
-        "data": [rights.json() for rights in role.rights_list],
+        "data": own_rights_list,
     }
 
 
@@ -111,9 +79,6 @@ def change_role_rights(rid):
     rights_obj_list = db.session.execute(
         db.select(RightsORM).where(RightsORM.id.in_(rights_list))
     ).all()
-    print(role.rights_list)
-    print([r[0] for r in rights_obj_list])
     role.rights_list = [r[0] for r in rights_obj_list]
     role.save()
-    print(role.rights_list)
     return {"code": 0, "message": "授权成功"}
