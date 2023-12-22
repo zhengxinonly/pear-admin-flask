b5625ecf1f996c49f436622f4cc512f0f06e05e2
zhengxinonly <pyxxponly@gmail.com>
2023-12-22 23:01:45 +0800

    refactor(templates): 移除 jinja2 includes 使用

--- pear_admin/extensions/init_jwt.py
+++ pear_admin/extensions/init_jwt.py
@@ -14,7 +14,7 @@ def user_identity_lookup(user):
 @jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
-    return UserORM.query.filter_by(id=identity).one_or_none()
+    return UserORM.query.filter(UserORM.id == identity).one_or_none()
 
 
 @jwt.expired_token_loader
---
+++ static/js/pear_admin_flask.js
@@ -0,0 +1,10 @@
+function getCookie(name) {
+  let matches = document.cookie.match(
+    new RegExp(
+      "(?:^|; )" +
+        name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+        "=([^;]*)",
+    ),
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
